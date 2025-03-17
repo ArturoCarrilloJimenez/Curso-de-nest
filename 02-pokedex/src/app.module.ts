d88955def2b8a850f9afcff99f396f371d10f2e2
ArturoCarrilloJimenez <arturojimenezalmontaras@gmail.com>
2025-03-17 14:04:01 +0100

    Cord básico con una base de datos mongo desplegada en mongo Atlas

--- 02-pokedex/src/app.module.ts
+++ 02-pokedex/src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
+    MongooseModule.forRoot(
+      // La db se encuentra en Mongo Atlas alojado de forma gratuita
+      `mongodb+srv://acarrilloj05:PruebasNest123@pruebas-nest.js903.mongodb.net/?retryWrites=true&w=majority&appName=Pruebas-Nest`,
+    ),
+
+    PokemonModule,
+    CommonModule,
+  ],
   controllers: [],
   providers: [],
 })
