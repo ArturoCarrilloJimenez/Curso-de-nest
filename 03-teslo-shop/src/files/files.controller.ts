901fbbaf42d6b04c4f116491d140befb261be6d9
ArturoCarrilloJimenez <arturojimenezalmontaras@gmail.com>
2025-03-24 11:47:30 +0100

    subida de imágenes y comienzo de obtención de estos

--- 03-teslo-shop/src/files/files.controller.ts
+++ 03-teslo-shop/src/files/files.controller.ts
@@ -1,7 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
+import { DirNameFile } from './interfaces/fileType.interface';
+import { DirNameFilePipe } from './pipes/dir-name-file/dir-name-file.pipe';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get(':type/:fileName')
+  findProductImage(
+    @Param('type', DirNameFilePipe) type: DirNameFile,
+    @Param('fileName') fileName: string,
+  ) {
+    return this.filesService.getStaticFile(type, fileName);
+  }
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/products',
+        filename: fileNamer,
+      }),
+    }),
+  ) // Interceptor
+  uploadFile(
+    @UploadedFile()
+    file: Express.Multer.File, // Si usas de base Express este es el tipo file
+  ) {
+    if (!file) throw new BadRequestException('file is missing');
+
+    return {
+      secureUrl: `http://localhost:3000/api/v1/files/product/${file.filename}`,
+    };
+  }
 }
--- 03-teslo-shop/src/files/files.service.ts
+++ 03-teslo-shop/src/files/files.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DirNameFile } from './interfaces/fileType.interface';
+import { join } from 'path';
+import { existsSync } from 'fs';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  constructor() {}
+
+  getStaticFile(type: DirNameFile, fileName: string) {
+    const path = join(`${__dirname}../../static/${type}/${fileName}`);
+
+    if (!existsSync(path))
+      throw new NotFoundException(
+        `File of type ${type} and name ${fileName} not find`,
+      );
+
+    return path;
+  }
+}
